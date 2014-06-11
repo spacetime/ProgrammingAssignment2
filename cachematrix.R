86934455a13aca68db97dd15f07b97b207387d41
Rishab Arora <ra.rishab@gmail.com>
2014-06-11 12:14:37 +0530

    Added definitions for makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xInv <- NULL
+    set <- function(y) {
+        x <<- y
+        xInv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) xInv <<- inv
+    getinverse <- function() xInv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## Assumptions: x is the input matrix, x is a square invertible matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if (!is.null(inv)) {
+        message("Returning cached inverse")
+        return(inv)
+    }
+    xmatrix <- x$get()
+    inv <- solve(xmatrix)
+    x$setinverse(inv)
+    inv    
 }
